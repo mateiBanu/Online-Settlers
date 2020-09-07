78713e6808cb74420447aca2cf0f0f4fc9176346
mateiBanu <mateibanu@yahoo.com>
2020-09-08 02:13:33 +0300

    Add dice pictures

--- client.py
+++ client.py
@@ -1,9 +1,14 @@
 import pygame
 import os
+import sys
 
-WIDTH = 1000
-HEIGHT = 700
+WIDTH = 1024
+HEIGHT = 768
 CAPTION = "Online Settlers"
+
+BACK_COLOR = (145, 145, 145)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 
 # Initialize pygame and create window
 pygame.init()
@@ -15,7 +20,33 @@ font = pygame.font.SysFont("arial", 12)
 
 CLOCK = pygame.time.Clock()
 
+def data(s):
+    return "imgs/"+s
 
-def show_dice(a, b):
-    pass
+class Dice:
+
+    def __init__(self):
+        pass
+
+    def update(self, a, b):
+        pass
+
+    def draw(self):
+        pass
+# Exit the program
+def done():
+    pygame.quit()
+    sys.exit()
+
+
+running = True
+while running:
+    SCREEN.fill(BACK_COLOR)
+    dt = CLOCK.tick(360)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done()
+    pygame.draw.rect(SCREEN, RED, (0,0, 40, 40))
+    pygame.display.update()
 
