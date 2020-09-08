083c1dce5e89abee9febdc9175d3ce6853c5cebd
mateiBanu <mateibanu@yahoo.com>
2020-09-08 22:10:32 +0300

    Implement buttons

--- client.py
+++ client.py
@@ -13,26 +13,86 @@ BLUE = (0, 0, 255)
 # Initialize pygame and create window
 pygame.init()
 os.environ['SDL_VIDEO_CENTERED'] = '1'
-SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption(CAPTION)
 pygame.font.init()
 font = pygame.font.SysFont("arial", 12)
 
-CLOCK = pygame.time.Clock()
+clock = pygame.time.Clock()
+
 
 def data(s):
-    return "imgs/"+s
+    return "imgs/" + s
+
+
+class Button:
+
+    def __init__(self, rect):
+        self.rect = rect
+        self.enabled = True
+        self.e_sprite = pygame.image.load(data("dice_button.png"))
+        self.d_sprite = pygame.image.load(data("dice_empty.png"))
+
+    def draw(self):
+        if self.enabled:
+            screen.blit(self.e_sprite, self.rect)
+        else:
+            screen.blit(self.d_sprite, self.rect)
+
+
+class DiceButton(Button):
+
+    def press(self):
+        pass
+
 
 class Dice:
 
     def __init__(self):
-        pass
+        self.val1 = 1
+        self.val2 = 1
+        self.x = 944
+        self.y = 600
+        self.rect1 = (self.x, self.y, self.x + 40, self.y + 40)
+        self.rect2 = (self.x + 40, self.y, self.x + 80, self.y + 40)
+        self.button = DiceButton((self.x, self.y, self.x + 80, self.y + 40))
+        self.sprite = []
+        for i in range(0, 7):
+            self.sprite.append("")
+        for i in range(1, 7):
+            self.sprite[i] = pygame.image.load(data("dice" + str(i) + ".png"))
 
     def update(self, a, b):
+        self.val1 = a
+        self.val2 = b
+
+    def draw(self):
+        screen.blit(self.sprite[self.val1], self.rect1)
+        screen.blit(self.sprite[self.val2], self.rect2)
+        self.button.draw()
+
+
+dice = Dice()
+
+
+class BuildRow:
+
+    def __init__(self):
         pass
 
     def draw(self):
         pass
+
+
+class BuildCard:
+
+    def __init__(self):
+        pass
+
+    def draw(self):
+        pass
+
+
 # Exit the program
 def done():
     pygame.quit()
@@ -41,12 +101,12 @@ def done():
 
 running = True
 while running:
-    SCREEN.fill(BACK_COLOR)
-    dt = CLOCK.tick(360)
+    screen.fill(BACK_COLOR)
+    dt = clock.tick(360)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done()
-    pygame.draw.rect(SCREEN, RED, (0,0, 40, 40))
+    dice.draw()
     pygame.display.update()
 
